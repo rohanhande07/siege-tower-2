28a6872f4733f1fd549f024928800a966dacb05b
rohanhande07 <64739098+rohanhande07@users.noreply.github.com>
2020-06-02 09:08:37 +0100

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,27 @@
+class Box extends BaseClass{
+
+    constructor(x, y, width, height) {
+      
+      super(x, y, width, height);
+      this.Visibility = 255;
+  
+    }
+
+    display(){
+
+      if(this.body.speed < 3){
+        super.display();
+      }
+
+      else{
+
+        World.remove(world,this.body);
+        push();
+        this.Visibility = this.Visibility - 5;
+        tint(255,this.Visibility);
+        pop();
+      }
+    }
+    
+  };
+  
